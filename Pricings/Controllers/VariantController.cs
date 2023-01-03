0a89006c9b4d226a7bb5dccb0787c536752b5c75
MelgreenTech <melgreentech@gmail.com>
2023-01-03 09:52:33 +0530

    Initial Commit

---
+++ Pricings/Controllers/VariantController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace Variants.Controllers
+{
+    [ApiController]
+    public class VariantController : ControllerBase
+    {        
+        public List<Variant> Variants { get; set; } = new List<Variant>() { 
+            new Variant() { 
+                ItemNumber = "G-0001",
+                VariantName = "Small",
+                RegularPrice = 399.00, 
+                SalesPrice = 299.00 
+            },
+            new Variant() {
+                ItemNumber = "G-0001",
+                VariantName = "Medium",
+                RegularPrice = 499.99,
+                SalesPrice = 399.00
+            },
+            new Variant() {
+                ItemNumber = "G-0001",
+                VariantName = "Large",
+                RegularPrice = 599.99,
+                SalesPrice = 499.00
+            }
+        };
+       
+
+        [HttpGet]
+        [Route("[controller]/{ItemNumber}")]
+        public async Task<List<Variant>> GetVariant(string ItemNumber)
+        {
+            if(ItemNumber == "G-0001")            
+                return Variants;
+            
+            throw new HttpRequestException();
+        }
+    }
+}
---
+++ Pricings/Variant.cs
@@ -0,0 +1,10 @@
+﻿namespace Variants
+{
+    public class Variant
+    {
+        public string ItemNumber { get; set; }
+        public string VariantName { get; set; }
+        public double SalesPrice { get; set; }
+        public double RegularPrice { get; set; }
+    }
+}
